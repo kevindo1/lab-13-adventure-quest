fc64035f131f692c3da991e04dcf317c77eb2c6b
Kevin Do <kevind0501@gmail.com>
2021-10-26 10:19:35 -0700

    added setUser and generateUser tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,45 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { setUser, generateUser } from "../utils.js";
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('generateUser should generate the formData with name, race, gold, and hp', (expect) => {
     //Arrange
+
+    const formData = new FormData();
+    formData.set('name', 'test name');
+    formData.set('race', 'tanjiro');
+
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = {
+        completed: {},
+        gold: 0,
+        hp: 50,
+        name: formData.get('name'),
+        race: formData.get('race'),
+    };
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = generateUser(formData);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.deepEqual(actual, expected);
+});
+
+test('setUser should save the information into localStorage', (expect) => {
+    localStorage.removeItem('USER');
+    const expected = {
+        completed: {},
+        gold: 0,
+        hp: 50,
+        name: 'test name',
+        race: 'tanjiro',
+    };
+
+    setUser(expected);
+    const actual = JSON.parse(localStorage.getItem('USER'));
+
+    expect.deepEqual(actual, expected);
 });
